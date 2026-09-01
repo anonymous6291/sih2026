6fbcb560788b3a35cde55be54aebde5a5be10b70
anonymous6291 <anishkumarshaw02@gmail.com>
2026-09-01 11:13:06 +0530

    Init configs.

---
+++ src/main/java/sih2026/Sih2026Application.java
@@ -0,0 +1,13 @@
+package sih2026;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sih2026Application {
+
+	static void main(String[] args) {
+		SpringApplication.run(Sih2026Application.class, args);
+	}
+
+}
